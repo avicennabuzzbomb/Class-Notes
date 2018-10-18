628ed0dc10aca0a8290057160ee022d65cd6a478
Matthew Ryan Blackburn <mrblackburn@wisc.edu>
2018-10-17 22:33:12 -0500

    more class notes: on beginning with Python

---
+++ pyNotes.sh
@@ -0,0 +1,12 @@
+10/17/2018
+* basic types: integers int vs. numbers with decimals float, and their binary representations
+* dot notation: `module.function` or `object.method`
+* No dots in variable names!
+* python is 0-indexed (unlike R, but like C, Perl, Java). index: offset from first value, or # steps
+* slice 0:4 has 0,1,2,3: 4-0=4 elements. think of it as [0-4[.
+* last index: -1, second to last: -2
+* [row, column] for numpy arrays
+
+* how to get help:
+* module.<tab>, variable.<tab>, dir(variable)to list available functions / methods
+* help(function_name) or ?function_name or ?variable.method_name
